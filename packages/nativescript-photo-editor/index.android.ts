05100bb34b87148b7059b268170054cd547528db
Igor Bezlepkin <hellobezlepkin@gmail.com>
2024-08-15 19:25:52 +0300

    Updated photo-editor plugin for android

--- packages/nativescript-photo-editor/index.android.ts
+++ packages/nativescript-photo-editor/index.android.ts
@@ -1,4 +1,4 @@
-import { AndroidActivityResultEventData, Application, File, ImageSource, knownFolders, path } from '@nativescript/core';
+import { AndroidActivityResultEventData, Application, File, ImageAsset, knownFolders, path } from '@nativescript/core';
 import { EditPhotoOptions, PhotoEditorCommon, PhotoEditorException } from './common';
 
 declare const com: any;
@@ -6,67 +6,60 @@ declare const com: any;
 export class PhotoEditor extends PhotoEditorCommon {
   private static readonly EDIT_PHOTO_REQUEST = 9090;
 
-  private tempFilepath: string;
-  private currentResolve: (result: ImageSource) => void;
-  private currentReject: (e: Error) => void;
+  private _tempFilepath: string;
+  private _currentResolve: (result: ImageAsset) => void;
+  private _currentReject: (e: Error) => void;
 
-  editPhoto(options: EditPhotoOptions): Promise<ImageSource> {
+  editPhoto(options: EditPhotoOptions): Promise<ImageAsset> {
     if (!options.imageSource) {
-      throw new PhotoEditorException('Required option "imageSource" no passed');
+      throw new PhotoEditorException('PhotoEditor error. Required option "imageSource" no passed');
     }
 
     options.hiddenControls = options.hiddenControls || [];
 
-    return new Promise<ImageSource>((resolve, reject) => {
-      this.currentResolve = resolve;
-      this.currentReject = reject;
-      this.tempFilepath = path.join(knownFolders.temp().path, `${Math.floor(new Date().getTime() / 1000)}.jpg`);
-      options.imageSource.saveToFile(this.tempFilepath, 'jpg');
+    return new Promise<ImageAsset>((resolve, reject) => {
+      this._currentResolve = resolve;
+      this._currentReject = reject;
+      this._tempFilepath = path.join(knownFolders.temp().path, `${Math.floor(new Date().getTime() / 1000)}.jpg`);
+
+      options.imageSource.saveToFile(this._tempFilepath, 'jpg');
 
       const intent = new android.content.Intent(Application.android.foregroundActivity, com.bezlepkin.photoeditor.PhotoEditorActivity.class);
-      intent.putExtra('imagePath', this.tempFilepath);
-      //intent.putExtra('isCropIn', options.hiddenControls.indexOf(PhotoEditorControl.Crop) === -1);
-      //intent.putExtra('isDrawIn', options.hiddenControls.indexOf(PhotoEditorControl.Draw) === -1);
-      //intent.putExtra('isTextIn', options.hiddenControls.indexOf(PhotoEditorControl.Text) === -1);
-      //intent.putExtra('isSaveIn', false);
-      // intent.putExtra('isClearIn', options.hiddenControls.indexOf(PhotoEditorControl.Clear) === -1);
+      intent.putExtra('imagePath', this._tempFilepath);
+
       Application.android.foregroundActivity.startActivityForResult(intent, PhotoEditor.EDIT_PHOTO_REQUEST);
-      Application.android.on('activityResult', this.onActivityResult);
+      Application.android.on('activityResult', this._onActivityResult);
     });
   }
 
-  private onActivityResult = (args: AndroidActivityResultEventData) => {
+  private _onActivityResult = (args: AndroidActivityResultEventData) => {
     if (args.requestCode === PhotoEditor.EDIT_PHOTO_REQUEST) {
       switch (args.resultCode) {
         case android.app.Activity.RESULT_OK:
           const resultIntent: android.content.Intent = args.intent;
           const filepath = resultIntent.getExtras().getString('filepath');
-          const imageSource = ImageSource.fromFileSync(filepath);
-
-          File.fromPath(filepath).removeSync();
-
-          this.currentResolve(imageSource);
+          this._currentResolve(new ImageAsset(filepath));
           break;
 
         case android.app.Activity.RESULT_CANCELED:
-          this.currentResolve(void 0);
+          this._currentResolve(void 0);
           break;
 
         default:
-          this.currentReject(new PhotoEditorException(`There was an unknown error while editing a photo with code: ${args.resultCode}`));
+          this._currentReject(new PhotoEditorException(`There was an unknown error while editing a photo with code: ${args.resultCode}`));
           break;
       }
 
       // Cleanup source temp file
-      if (this.tempFilepath && File.exists(this.tempFilepath)) {
-        File.fromPath(this.tempFilepath).removeSync();
+      if (this._tempFilepath && File.exists(this._tempFilepath)) {
+        File.fromPath(this._tempFilepath).removeSync();
       }
 
       // Cleanup events & current vars
-      this.tempFilepath = undefined;
-      this.currentResolve = undefined;
-      this.currentReject = undefined;
-      Application.android.off('activityResult', this.onActivityResult);
+      this._tempFilepath = undefined;
+      this._currentResolve = undefined;
+      this._currentReject = undefined;
+      Application.android.off('activityResult', this._onActivityResult);
     }
   };
 }
